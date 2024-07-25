4956bed436e6bad1f368e69a3636ca581d567a66
suhwan <tnghkswkd123@naver.com>
2024-07-25 17:50:15 +0900

    feat: GlobalExceptionHandler 에 default Exception 부분 추가

--- accounts/src/main/java/com/eazybytes/accounts/exception/GlobalExceptionHandler.java
+++ accounts/src/main/java/com/eazybytes/accounts/exception/GlobalExceptionHandler.java
@@ -12,6 +12,19 @@ import java.time.LocalDateTime;
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
+	@ExceptionHandler(Exception.class)
+	public ResponseEntity<ErrorResponseDto> handleGlobalException(Exception exception,
+	                                                                        WebRequest webRequest) {
+		ErrorResponseDto errorResponseDto = new ErrorResponseDto(
+				webRequest.getDescription(false),
+				HttpStatus.INTERNAL_SERVER_ERROR,
+				exception.getMessage(),
+				LocalDateTime.now()
+		);
+
+		return new ResponseEntity<>(errorResponseDto, HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+
 	@ExceptionHandler(ResourceNotFoundException.class)
 	public ResponseEntity<ErrorResponseDto> handleResourceNotFoundException(ResourceNotFoundException exception,
 	                                                                             WebRequest webRequest) {
@@ -22,7 +35,7 @@ public class GlobalExceptionHandler {
 				LocalDateTime.now()
 		);
 
-		return new ResponseEntity<>(errorResponseDto, HttpStatus.BAD_REQUEST);
+		return new ResponseEntity<>(errorResponseDto, HttpStatus.NOT_FOUND);
 	}
 
 	@ExceptionHandler(CustomerAlreadyExistsException.class)

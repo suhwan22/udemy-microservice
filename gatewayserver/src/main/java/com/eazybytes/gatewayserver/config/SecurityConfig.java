190f38ff86c0496aef74710b84bb34cda18da000
suhwan <tnghkswkd123@naver.com>
2024-10-02 01:48:42 +0900

    feat: SecurityConfig 변경
    1. accounts, cards, loans 에 각각 ACCOUNTS, CARDS, LOANDS 라는role이
       있어야 요청이 가능하도록 변경

--- gatewayserver/src/main/java/com/eazybytes/gatewayserver/config/SecurityConfig.java
+++ gatewayserver/src/main/java/com/eazybytes/gatewayserver/config/SecurityConfig.java
@@ -2,11 +2,17 @@ package com.eazybytes.gatewayserver.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.ReactiveJwtAuthenticationConverterAdapter;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import reactor.core.publisher.Mono;
 
 @Configuration
 @EnableWebFluxSecurity
@@ -15,13 +21,22 @@ public class SecurityConfig {
 	@Bean
 	public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity serverHttpSecurity) {
 		serverHttpSecurity.authorizeExchange((auth)->auth.pathMatchers(HttpMethod.GET).permitAll()
-				.pathMatchers("/eazybank/accounts/**").authenticated()
-				.pathMatchers("/eazybank/cards/**").authenticated()
-				.pathMatchers("/eazybank/loans/**").authenticated())
-				.oauth2ResourceServer((oauth)->oauth.jwt(Customizer.withDefaults()));
+				.pathMatchers("/eazybank/accounts/**").hasRole("ACCOUNTS")
+				.pathMatchers("/eazybank/cards/**").hasRole("CARDS")
+				.pathMatchers("/eazybank/loans/**").hasRole("LOANS"))
+				.oauth2ResourceServer((oauth)->oauth
+						.jwt((jwtSpec -> jwtSpec.jwtAuthenticationConverter(grantedAuthoritiesExtractor()))));
 
 		serverHttpSecurity.csrf((auth)->auth.disable());
 
 		return serverHttpSecurity.build();
+	}
+
+	private Converter<Jwt, Mono<AbstractAuthenticationToken>> grantedAuthoritiesExtractor() {
+		JwtAuthenticationConverter jwtAuthenticationConverter =
+				new JwtAuthenticationConverter();
+		jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter
+				(new KeycloakRoleConverter());
+		return new ReactiveJwtAuthenticationConverterAdapter(jwtAuthenticationConverter);
 	}
 }
